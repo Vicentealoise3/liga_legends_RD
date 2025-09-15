afa293802487404dc715f18fc7e7b70c0492d891
Vicentealoise3 <vicentealoise3@gmail.com>
2025-09-15 20:37:18 -0300

    Actualizar resultados.js

--- resultados.js
+++ resultados.js
@@ -2,7 +2,7 @@
 const resultados = {
   "Cuartos - Serie 1": {equipos:["Brewers","Mets"], juegos:["7-0","5-0",""], ronda:"cuartos"},
   "Cuartos - Serie 2": {equipos:["Tigers","Dodgers"], juegos:["6-4","5-8",""], ronda:"cuartos"},
-  "Cuartos - Serie 3": {equipos:["Blue Jays","Mariners"], juegos:["1-3","",""], ronda:"cuartos"},
+  "Cuartos - Serie 3": {equipos:["Blue Jays","Mariners"], juegos:["1-3","1-2",""], ronda:"cuartos"},
   "Cuartos - Serie 4": {equipos:["Cubs","Padres"], juegos:["3-1","2-1",""], ronda:"cuartos"},
   "Semifinal - Serie 1": {equipos:["Brewers","Ganador QF2"], juegos:["","",""], ronda:"semifinal"},
   "Semifinal - Serie 2": {equipos:["Ganador QF3","Cubs"], juegos:["","",""], ronda:"semifinal"},
